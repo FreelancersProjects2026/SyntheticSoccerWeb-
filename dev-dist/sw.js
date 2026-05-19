9d420016a43f7f7ac8e98aaee4e1ee2a09ab6c21
jasonMB12 <jason.moyabre.es@gmail.com>
2026-05-18 19:31:48 -0600

    solving Pipeline

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j4jg12l4ju4"
+    "revision": "0.011f3vl0j4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
