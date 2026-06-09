8b642ce0ba2183ef3aef6f741ceae4d60b9b29c6
jasonMB12 <jason.moyabre.es@gmail.com>
2026-06-09 12:08:33 -0600

    reglas PR

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6j8spu646q"
+    "revision": "0.243g462k9kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
