ec581c921e6303f841aac600f5b449a99ac3bc89
jasonMB12 <jason.moyabre.es@gmail.com>
2026-07-12 11:10:48 -0600

    client.ts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7ep6u8pfte8"
+    "revision": "0.upvshl8cr8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
